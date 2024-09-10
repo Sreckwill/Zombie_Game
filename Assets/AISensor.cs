17189adaa5754c1e849ee73332f7e79da9fca27a
Sreckwill <162847277+Sreckwill@users.noreply.github.com>
2024-09-10 19:22:45 +0530

    add Script

---
+++ Assets/AISensor.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISensor : MonoBehaviour
+{
+
+    public float distance = 10;
+
+    public float angle = 30;
+
+    public float height = 1.0f;
+
+    public Color meshColor=Color.red;
+
+    Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    Mesh CreateWedgeMesh()
+    {
+        Mesh mesh = new Mesh();
+        int numTriangles
+
+            return Mesh;
+    }
+}
