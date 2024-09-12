a7a2ea0b118c159668bf1084e8db0ab74c115266
Sreckwill <162847277+Sreckwill@users.noreply.github.com>
2024-09-12 16:22:28 +0530

    Add the Scene For MainMenu and GamePlay

---
+++ Assets/Scripts/UI Manager/UIManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void PlayScene()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
